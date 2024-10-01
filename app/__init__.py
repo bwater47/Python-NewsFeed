7dec299e5733556cc4387be5d0d1f12e437b90ec
B Water <brennw47@gmail.com>
2024-10-01 11:00:32 -0500

    feat(app): add filter registration and custom template filters  Added the registration of filters in the `app/init.py`. Also added custom filters to elements in the template for better functionality.  Refs: #3 Implement Filters in Template Files
    feat(app): add filter registration and custom template filters
    
    Added the registration of filters in the `app/init.py`.
    Also added custom filters to elements in the template for better functionality.
    
    Refs: #3 Implement Filters in Template Files

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.utils import filters
 from app.db import init_db
 from app.routes import home, dashboard
 from flask import Flask
@@ -9,6 +10,11 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+
+  # Register custom filters.
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   # Register the blueprints.
   app.register_blueprint(home)
